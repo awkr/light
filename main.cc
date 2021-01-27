7157b53f5543f48c9fc6e21b87e487bf762b5fd6
L7 <zhu.life@gmail.com>
2021-01-27 12:39:48 +0800

    init instance

--- main.cc
+++ main.cc
@@ -1,1 +1,26 @@
-int main() {}
+#include <iostream>
+#include <vulkan/vulkan.hpp>
+
+const char *const kAppName = "Light";
+const char *const kEngineName = "Vulkan";
+
+int main() {
+    try {
+        vk::ApplicationInfo applicationInfo(kAppName, 1, kEngineName, 1,
+                                            VK_API_VERSION_1_2);
+        vk::InstanceCreateInfo instanceCreateInfo({}, &applicationInfo);
+        vk::UniqueInstance instance =
+                vk::createInstanceUnique(instanceCreateInfo);
+    } catch (vk::SystemError &err) {
+        std::cerr << "vk::SystemError: " << err.what() << std::endl;
+        exit(EXIT_FAILURE);
+    } catch (std::exception &ex) {
+        std::cerr << "std::exception: " << ex.what() << std::endl;
+        exit(EXIT_FAILURE);
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
+}
